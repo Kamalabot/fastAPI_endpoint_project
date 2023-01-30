672a49245c762c4adb6160b96330c714f7e889da
solverbot <kamaljp@gmail.com>
2023-01-30 18:37:32 +0530

    completed the CRUD using ORM

--- src/main_withORM.py
+++ src/main_withORM.py
@@ -30,76 +30,63 @@ async def root():
     #is returned.
     return {"db_server":"This is from Database Server"}
 
-@app.get("/sqlormtest")
+#The other /posts has been deleted 
+@app.get("/posts")
 def get_ormres(db: Session = Depends(get_db)):
     #querying the database session
     data_posts = db.query(models.Post).all()
     return {"db_post":data_posts}
 
-#below handle informs path of the website
-@app.get("/posts")
-def get_posts():
-    #Query the table 
-    
-    cur.execute("SELECT * FROM social_posts")
-    
-    #get the data into templist
-    
-    postsList = cur.fetchall()
-    return {"YourPosts":"There you go. Your posts",
-            "data":postsList}
 #lets make createposts useful and load data into local_posts array
 
 @app.post("/addposts",status_code=status.HTTP_201_CREATED)
-def add_posts(load: Newposts):
-    print(load)
-    #Appending the post to local array
-    cur.execute("""INSERT INTO social_posts(title, content, is_published)
-                VALUES(%s, %s, %s) RETURNING * """,(load.title,load.content,
-                                                     load.is_published))
-    updated_post = cur.fetchone()
-    #returning the response along with the array data
-    return{"message":updated_post}
+def add_posts(load: Newposts,db: Session = Depends(get_db)):
+
+#**load.dict() will take care of converting the variables in the format models will accept
+    new_post = models.Post(**load.dict())
+    #new_post = models.Post(title=load.title, content=load.content, is_published=load.is_published)
+#need to present the query to engine
+    db.add(new_post)
+#query needs to be comitted
+    db.commit()
+#After that the refreshed database needs to be stored back in to the variable
+    db.refresh(new_post)
+    return{"message":new_post}
 
 @app.get("/params/latest")
-def get_recent():
-    cur.execute("""SELECT * FROM social_posts 
-                ORDER BY post_id DESC
-                LIMIT 1""")
-    recent_post = cur.fetchone()
-    return {"Recent Post":recent_post}
+def get_recent(db: Session = Depends(get_db)):
+    data_posts = db.query(models.Post).order_by('post_id').all()
+    return {"Recent Post":data_posts}
 
 @app.get("/params/{prm}")
 #Note the :int, this will validate the parameter
-def print_params(prm :int, response: Response):
-    print(prm)
-    #There is a gotcha here. The id has to be string when sending the query.
-    #There is an extra comman after str(prm), for some reason!!!
-    cur.execute("""SELECT * FROM social_posts WHERE post_id = %s""",(str(prm),))
-    requestedPost = cur.fetchone()
-    if not requestedPost:
+def print_params(prm :int, db: Session = Depends(get_db)):
+    one_row = db.query(models.Post).filter(models.Post.post_id == prm).first()
+    if not one_row:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'The id {prm} is not found')
-    return {"message":"Got post","requested_post":requestedPost}
+    return {"message":"Got post","requested_post":one_row}
 
 @app.delete("/delete/{id}",status_code = status.HTTP_204_NO_CONTENT)
 #when using the 204 status, the remaining posts or any data cannot be sent
-def delete_posts(id :int):
-    cur.execute("""DELETE FROM social_posts WHERE post_id = %s RETURNING *""",(str(id))) 
-    deleted = cur.fetchone()
-    if deleted == None:
+def delete_posts(id :int,db: Session = Depends(get_db)):
+    one_row = db.query(models.Post).filter(models.Post.post_id == id) 
+    if one_row.first() == None:
         raise(HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="the id is not found"))
+    one_row.delete(synchronize_session = False)
+    db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/update/{id}",status_code=status.HTTP_201_CREATED)
-def update_post(id :int,post:Newposts):
-    cur.execute("""UPDATE social_posts SET title = %s,content = %s WHERE post_id = %s RETURNING *""",
-                 (post.title,post.content,str(id)))
-    executed = cur.fetchone()
-    if executed == None:
+def update_post(id :int,post:Newposts,db: Session = Depends(get_db)):
+    save_query = db.query(models.Post).filter(models.Post.post_id == id)
+    if save_query.first()== None:
         raise(HTTPException(status_code=status.HTTP_404_NOT_FOUND))
+
+    save_query.update(post.dict(),synchronize_session=False)
+    db.commit()
 
     #update the id in the recieved data and replace that in the local_posts
     return {'message':"data updated",
-            "data":executed} 
+            "data":save_query.first()} 
