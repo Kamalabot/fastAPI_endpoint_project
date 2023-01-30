5cad35341d35db18da28c0656395fab67aed922a
solverbot <kamaljp@gmail.com>
2023-01-30 10:12:39 +0530

    understood posts with pydantic

--- src/main.py
+++ src/main.py
@@ -3,6 +3,14 @@
 #This is the trial script learning the fastapi library
 
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+
+#In order to enforce schema on the post requests following class is declared
+class Newposts(BaseModel):
+    title:str
+    content:str
+    category:int
 
 app = FastAPI()
 
@@ -18,3 +26,17 @@ async def root():
 @app.get("/posts")
 def get_posts():
     return {"YourPosts":"There you go. Your posts"}
+'''
+#lets work on the POST request without pydantic
+@app.post("/createposts")
+def make_posts(payLoad: dict = Body):
+    print(payLoad)
+    return {"message":"SUCCESS"}
+'''
+
+#lets work on the POST request withpydantic
+@app.post("/createposts")
+def make_pydanticposts(load: Newposts):
+    print(load)
+    print(f"The load title is {load.title}")
+    return {"message":"Success with Pydantic","your post":load}
